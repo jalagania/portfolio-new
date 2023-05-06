7e07b0e2caadd5ef362432c918a635c1e52136d1
jalagania <beka>
2023-05-06 19:25:16 +0400

    added contact section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,26 @@ function Footer() {
     <footer id="contact" className="footer">
       <div className="section-container">
         <h3 className="section-heading">Contact</h3>
-        {/* <p className="copyright">&copy; 2023 Beka Jalagania</p> */}
+        <div className="form-container">
+          <h4>Get in touch!</h4>
+          <form
+            // action="https://formsubmit.co/beka.jalagania@gmail.com"
+            // method="POST"
+            className="footer__form"
+            onClick={(event) => event.preventDefault()}
+          >
+            <input type="text" name="name" placeholder="Name" className="footer__form--input" />
+            <input type="email" name="email" placeholder="Email" className="footer__form--input" />
+            <textarea
+              name="message"
+              placeholder="Write your message here"
+              className="footer__form--textarea"
+            ></textarea>
+            <button className="footer__form--btn-submit">Send</button>
+          </form>
+        </div>
       </div>
+      <p className="footer__copyright">&copy; 2023 Beka Jalagania</p>
     </footer>
   );
 }
