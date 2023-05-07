e63cc92ac582486e18586df825c509181611c8c2
jalagania <beka>
2023-05-07 17:43:39 +0400

    added mailjs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,42 @@
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
 import "./Footer.css";
 
 function Footer() {
+  const formRef = useRef();
+  const [notification, setNotification] = useState({ name: "", text: "" });
+
+  function handleSubmitButton() {
+    emailjs
+      .sendForm("service_pz7wz3a", "template_yzf4vaw", formRef.current, "fKqB4QRxcolYhM4qR")
+      .then(
+        // eslint-disable-next-line no-unused-vars
+        (result) => {
+          setNotification({ name: "success", text: "Message has been sent" });
+        },
+        // eslint-disable-next-line no-unused-vars
+        (error) => {
+          setNotification({ name: "error", text: "Couldn't send message. Try again" });
+        }
+      );
+  }
+
   return (
     <footer id="contact" className="footer">
       <div className="section-container">
         <h3 className="section-heading">Contact</h3>
-        <div className="form-container">
+        <div className="footer-form-container">
           <h4>Get in touch!</h4>
-          <form className="footer__form" onSubmit={(event) => event.preventDefault()}>
-            <input type="text" name="name" placeholder="Name" className="footer__form--input" />
-            <input type="email" name="email" placeholder="Email" className="footer__form--input" />
+          <h5 className="footer-form__notification">{notification.text}</h5>
+          <form ref={formRef} className="footer-form" onSubmit={(event) => event.preventDefault()}>
+            <input type="text" name="name" placeholder="Name" className="footer-form__input" />
+            <input type="email" name="email" placeholder="Email" className="footer-form__input" />
             <textarea
               name="message"
               placeholder="Write your message here"
-              className="footer__form--textarea"
+              className="footer-form__textarea"
             ></textarea>
-            <button type="submit" className="footer__form--btn-submit">
+            <button type="submit" className="footer-form__btn-submit" onClick={handleSubmitButton}>
               Send
             </button>
           </form>
